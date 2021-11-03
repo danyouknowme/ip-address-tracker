c74eac2018f049190e6114dfb19a6d8c29e496be
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-03 14:19:32 +0700

    create map and title component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./App.scss";
 
 const App = () => {
   return (
     <div className="App">
-      
+      <div className="wrapper">
+        <div className="title">
+          <div className="bg"></div>
+        </div>
+        <div className="map">
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
